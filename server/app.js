374d007e8fda07a150cbac6d393a6b74168847eb
Olufemi Oyewole <olufemi.oyewole@andela.com>
2017-03-23 12:42:59 +0100

    chore(api): install body-parser and morgan

--- server/app.js
+++ server/app.js
@@ -1,6 +1,16 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import logger from 'morgan';
 
+// Set up the express app
 const app = express();
+
+// Log requests to the console.
+app.use(logger('dev'));
+
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send('Document Management System');
